b10dda6d0b99a9ad249bcb0a52a523ae90d0ae87
susana moreira <suzanasuzy12@hotmail.com>
2023-03-23 16:53:40 -0300

    subindo projeto desenvolvido durante o curso

---
+++ tests/tasks.spec.ts
@@ -0,0 +1,75 @@
+import { expect, test } from '@playwright/test';
+import { TaskModel } from './fixtures/task.model';
+import { deleteTaskByHelper, postTask } from './support/helpers';
+import { TasksPage } from './support/pages/tasks'
+import data from './fixtures/tasks.json'
+
+let tasksPage: TasksPage
+
+test.beforeEach(({ page }) => {
+    tasksPage = new TasksPage(page)
+})
+
+test.describe('Cadastro', () => {
+    test('Deve poder cadastrar uma nova tarefa', async ({ request }) => {
+        const task = data.success as TaskModel
+
+        await deleteTaskByHelper(request, task.name);
+
+        await tasksPage.go();
+        await tasksPage.create(task);
+        await tasksPage.shouldHaveText(task.name);
+
+    })
+
+    test('Nao deve permitir tarefa duplicada', async ({ request }) => {
+        const task = data.duplicate as TaskModel
+
+        await deleteTaskByHelper(request, task.name);
+        await postTask(request, task);
+
+        await tasksPage.go();
+        await tasksPage.create(task);
+        await tasksPage.alertHaveText('Task already exists!');
+
+    })
+
+    test('Campo obrigatório', async () => {
+        const task = data.required as TaskModel
+
+        await tasksPage.go();
+        await tasksPage.create(task);
+
+        const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage);
+        expect(validationMessage).toEqual('This is a required field');
+    })
+})
+
+test.describe('Atualizacao', () => {
+    test('Deve concluir uma tarefa', async ({ request }) => {
+        const task = data.update as TaskModel
+
+        await deleteTaskByHelper(request, task.name);
+        await postTask(request, task);
+
+        await tasksPage.go()
+        await tasksPage.toggle(task.name)
+
+        await tasksPage.shouldBeDone(task.name)
+    })
+})
+
+test.describe('Exclusao', () => {
+    test('Deve concluir uma tarefa', async ({ request }) => {
+        const task = data.delete as TaskModel
+
+        await deleteTaskByHelper(request, task.name);
+        await postTask(request, task);
+
+        await tasksPage.go()
+        await tasksPage.remove(task.name)
+
+        await tasksPage.shouldNotExist(task.name)
+    })
+})
+
